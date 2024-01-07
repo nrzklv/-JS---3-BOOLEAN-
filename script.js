5559554da9190d6deace50889f2ad61059742c66
nsx_69 <mrmanafest16@gmail.com>
2024-01-07 15:22:38 +0500

    tasks from code mu

---
+++ script.js
@@ -0,0 +1,22 @@
+// let total;
+// alert(total);
+
+// let a = null;
+// alert(a);
+
+// let c = true;
+// alert(c);
+
+// let first = 'abc';
+// let second = 'def'
+// let sum = first * second;
+// alert(sum)
+
+// let first = 'abc';
+// let second = 123;
+// let sum = first * second;
+// alert(sum);
+
+// alert(10 / 0);
+
+// alert(-10 / 0);
